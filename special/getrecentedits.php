e61c70b6c5d22fa289d37f0dcb637ee9e1ac4863
weeklyd3 <i.hate.spam.mail.here+github@gmail.com>
2022-06-13 21:37:32 +0000

    That should do it

--- special/getrecentedits.php
+++ special/getrecentedits.php
@@ -10,6 +10,6 @@ fwrite(fopen(__DIR__ . '/../mod/recentChanges.json', 'w+'), json_encode($recentE
 <ul>
 <?php
 foreach ($recentEdits as $edit) {
-    ?><li>(<a href="index.php?title=Special:old&revision=<?php echo $edit->revid; ?>&page=<?php echo htmlspecialchars(urlencode($edit->title)); ?>">archive</a> | <a href="index.php?action=history&title=<?php echo htmlspecialchars(urlencode($edit->title)); ?>"></a>) <?php echo formatDate($edit->time); ?> <?php echo userlink(userinfo($edit->author)->username); ?> [[<a href="index.php?title=<?php echo htmlspecialchars(urlencode($edit->title)); ?>"><?php echo htmlspecialchars($edit->title); ?></a>]] (<?php echo htmlspecialchars($edit->summary); ?>)</li><?php
+    ?><li>(<a href="index.php?title=Special:old&revision=<?php echo $edit->revid; ?>&page=<?php echo htmlspecialchars(urlencode($edit->title)); ?>">archive</a> | <a href="index.php?action=history&title=<?php echo htmlspecialchars(urlencode($edit->title)); ?>">history</a>) <?php echo formatDate($edit->time); ?> <?php echo userlink(userinfo($edit->author)->username); ?> [[<a href="index.php?title=<?php echo htmlspecialchars(urlencode($edit->title)); ?>"><?php echo htmlspecialchars($edit->title); ?></a>]] (<?php echo htmlspecialchars($edit->summary); ?>)</li><?php
 }
 ?></ul><?php ;
